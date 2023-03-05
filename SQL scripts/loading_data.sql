4f78c5db48de630af8037acf377aae3295c78ce2
Arthur Timmermans <arthur.timmermans@student.hu.nl>
2023-03-05 22:46:31 +0100

    E

---
+++ SQL scripts/loading_data.sql
@@ -0,0 +1,11 @@
+SELECT dengue_cases, country 
+FROM dengue_tidy order 
+BY dengue_cases asc
+
+SELECT flu_cases, country 
+FROM flu_tidy order 
+BY flu_cases asc
+
+SELECT population, country 
+FROM gapminder order 
+BY population desc
